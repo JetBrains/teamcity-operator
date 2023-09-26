1d6aec42959835fbce061e74d720badf48a3b039
anton.aleksandrov <anton.aleksandrov@jetbrains.com>
2023-09-26 15:16:27 +0200

    TCI-403 removed const file

--- internal/resource/const.go
+++
@@ -1,7 +0,0 @@
-package resource
-
-const (
-	DATABASE_PROPERTIES_VOLUME_NAME = "database-properties"
-	DIR_SETUP_CONTAINER_NAME        = "dir-setup"
-	DIR_SETUP_CONTAINER_IMAGE       = "alpine:latest"
-)
