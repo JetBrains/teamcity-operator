c76b780dd1658e4535871b47cb6ab20b6c213267
anton.aleksandrov <anton.aleksandrov@jetbrains.com>
2023-10-03 15:07:34 +0200

    TCI-403 ignoring empty strings during secret data split

--- internal/validator/util.go
+++ internal/validator/util.go
@@ -3,5 +3,16 @@ package validator
 import "strings"
 
 func ByteArrayToLineStringArray(array []uint8) []string {
-	return strings.Split(string(array), "\n")
+	return deleteEmpty(strings.Split(string(array), "\n"))
+}
+
+func deleteEmpty(strarr []string) []string {
+	var arr []string
+	for _, str := range strarr {
+		if str != "" {
+			arr = append(arr, str)
+		}
+	}
+	return arr
+
 }
