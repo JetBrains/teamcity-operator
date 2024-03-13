daf7a3838da69798313c049fdf71db875a2cc6b0
anton.aleksandrov <anton.aleksandrov@jetbrains.com>
2024-03-13 11:29:19 +0100

    TCI-492 re-enabled tests + reworked main statefulset tests based on the new spec

--- internal/resource/persistentvolumeclaim_test.go
+++ internal/resource/persistentvolumeclaim_test.go
@@ -1,56 +1,55 @@
 package resource
 
-//
-//import (
-//	. "git.jetbrains.team/tch/teamcity-operator/api/v1beta1"
-//	. "github.com/onsi/ginkgo/v2"
-//	. "github.com/onsi/gomega"
-//	v12 "k8s.io/api/core/v1"
-//)
-//
-//var _ = Describe("PersistentVolumeClaim", func() {
-//	Context("TeamCity with required persistence", func() {
-//		BeforeEach(func() {
-//			BeforeEachBuild(func(teamcity *TeamCity) {})
-//		})
-//		It("creates a single PVC with correct settings", func() {
-//			objList, err := DefaultPersistentVolumeClaimBuilder.BuildObjectList()
-//			Expect(err).NotTo(HaveOccurred())
-//			Expect(len(objList)).To(Equal(1))
-//			obj := objList[0]
-//			err = DefaultPersistentVolumeClaimBuilder.Update(obj)
-//			Expect(err).NotTo(HaveOccurred())
-//			actual := obj.(*v12.PersistentVolumeClaim)
-//			expected := Instance.Spec.DataDirVolumeClaim
-//			Expect(actual.Spec).To(Equal(expected.Spec))
-//			Expect(actual.Annotations).To(Equal(expected.Annotations))
-//		})
-//	})
-//
-//	Context("TeamCity with additional persistence", func() {
-//		BeforeEach(func() {
-//			BeforeEachBuild(func(teamcity *TeamCity) {
-//				teamcity.Spec.PersistentVolumeClaims = []CustomPersistentVolumeClaim{getAdditionalPVC()}
-//			})
-//		})
-//		It("creates data dir PVC and one additional PVC with correct settings", func() {
-//			objList, err := DefaultPersistentVolumeClaimBuilder.BuildObjectList()
-//			Expect(err).NotTo(HaveOccurred())
-//			Expect(len(objList)).To(Equal(2))
-//			dataDirPersistentVolumeClaim := objList[0]
-//			err = DefaultPersistentVolumeClaimBuilder.Update(dataDirPersistentVolumeClaim)
-//			Expect(err).NotTo(HaveOccurred())
-//			actual := dataDirPersistentVolumeClaim.(*v12.PersistentVolumeClaim)
-//			expected := Instance.Spec.DataDirVolumeClaim
-//			Expect(actual.Spec).To(Equal(expected.Spec))
-//
-//			additionalPersistentVolumeClaim := objList[1]
-//			err = DefaultPersistentVolumeClaimBuilder.Update(additionalPersistentVolumeClaim)
-//			Expect(err).NotTo(HaveOccurred())
-//			actual = additionalPersistentVolumeClaim.(*v12.PersistentVolumeClaim)
-//			expected = Instance.Spec.PersistentVolumeClaims[0]
-//			Expect(actual.Spec).To(Equal(expected.Spec))
-//			Expect(actual.Annotations).To(Equal(expected.Annotations))
-//		})
-//	})
-//})
+import (
+	. "git.jetbrains.team/tch/teamcity-operator/api/v1beta1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	v12 "k8s.io/api/core/v1"
+)
+
+var _ = Describe("PersistentVolumeClaim", func() {
+	Context("TeamCity with required persistence", func() {
+		BeforeEach(func() {
+			BeforeEachBuild(func(teamcity *TeamCity) {})
+		})
+		It("creates a single PVC with correct settings", func() {
+			objList, err := DefaultPersistentVolumeClaimBuilder.BuildObjectList()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(objList)).To(Equal(1))
+			obj := objList[0]
+			err = DefaultPersistentVolumeClaimBuilder.Update(obj)
+			Expect(err).NotTo(HaveOccurred())
+			actual := obj.(*v12.PersistentVolumeClaim)
+			expected := Instance.Spec.DataDirVolumeClaim
+			Expect(actual.Spec).To(Equal(expected.Spec))
+			Expect(actual.Annotations).To(Equal(expected.Annotations))
+		})
+	})
+
+	Context("TeamCity with additional persistence", func() {
+		BeforeEach(func() {
+			BeforeEachBuild(func(teamcity *TeamCity) {
+				teamcity.Spec.PersistentVolumeClaims = []CustomPersistentVolumeClaim{getAdditionalPVC()}
+			})
+		})
+		It("creates data dir PVC and one additional PVC with correct settings", func() {
+			objList, err := DefaultPersistentVolumeClaimBuilder.BuildObjectList()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(objList)).To(Equal(2))
+			dataDirPersistentVolumeClaim := objList[0]
+			err = DefaultPersistentVolumeClaimBuilder.Update(dataDirPersistentVolumeClaim)
+			Expect(err).NotTo(HaveOccurred())
+			actual := dataDirPersistentVolumeClaim.(*v12.PersistentVolumeClaim)
+			expected := Instance.Spec.DataDirVolumeClaim
+			Expect(actual.Spec).To(Equal(expected.Spec))
+
+			additionalPersistentVolumeClaim := objList[1]
+			err = DefaultPersistentVolumeClaimBuilder.Update(additionalPersistentVolumeClaim)
+			Expect(err).NotTo(HaveOccurred())
+			actual = additionalPersistentVolumeClaim.(*v12.PersistentVolumeClaim)
+			expected = Instance.Spec.PersistentVolumeClaims[0]
+			Expect(actual.Spec).To(Equal(expected.Spec))
+			Expect(actual.Annotations).To(Equal(expected.Annotations))
+		})
+	})
+})
--- internal/resource/service_test.go
+++ internal/resource/service_test.go
@@ -1,42 +1,41 @@
 package resource
 
-//
-//import (
-//	. "git.jetbrains.team/tch/teamcity-operator/api/v1beta1"
-//	. "github.com/onsi/ginkgo/v2"
-//	. "github.com/onsi/gomega"
-//	v12 "k8s.io/api/core/v1"
-//)
-//
-//var _ = Describe("Service", func() {
-//	Context("TeamCity with service", func() {
-//		BeforeEach(func() {
-//			BeforeEachBuild(func(teamcity *TeamCity) {
-//				teamcity.Spec.ServiceList = getServiceList()
-//			})
-//		})
-//		It("sets a list of objects with proper length, names, and namespaces", func() {
-//			objList, err := DefaultServiceBuilder.BuildObjectList()
-//			desiredServiceList := Instance.Spec.ServiceList
-//			Expect(err).NotTo(HaveOccurred())
-//			Expect(len(objList)).To(Equal(len(desiredServiceList)))
-//			for idx, obj := range objList {
-//				svc := obj.(*v12.Service)
-//				Expect(svc.Name).To(Equal(desiredServiceList[idx].Name))
-//				Expect(svc.Namespace).To(Equal(TeamCityNamespace))
-//			}
-//		})
-//		It("updates objects' configuration properly", func() {
-//			objList, err := DefaultServiceBuilder.BuildObjectList()
-//			Expect(err).NotTo(HaveOccurred())
-//			for idx, obj := range objList {
-//				err = DefaultServiceBuilder.Update(obj)
-//				Expect(err).NotTo(HaveOccurred())
-//				actual := obj.(*v12.Service)
-//				expected := Instance.Spec.ServiceList[idx]
-//				Expect(actual.Annotations).To(Equal(expected.Annotations))
-//				Expect(actual.Spec).To(Equal(expected.ServiceSpec))
-//			}
-//		})
-//	})
-//})
+import (
+	. "git.jetbrains.team/tch/teamcity-operator/api/v1beta1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	v12 "k8s.io/api/core/v1"
+)
+
+var _ = Describe("Service", func() {
+	Context("TeamCity with service", func() {
+		BeforeEach(func() {
+			BeforeEachBuild(func(teamcity *TeamCity) {
+				teamcity.Spec.ServiceList = getServiceList()
+			})
+		})
+		It("sets a list of objects with proper length, names, and namespaces", func() {
+			objList, err := DefaultServiceBuilder.BuildObjectList()
+			desiredServiceList := Instance.Spec.ServiceList
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(objList)).To(Equal(len(desiredServiceList)))
+			for idx, obj := range objList {
+				svc := obj.(*v12.Service)
+				Expect(svc.Name).To(Equal(desiredServiceList[idx].Name))
+				Expect(svc.Namespace).To(Equal(TeamCityNamespace))
+			}
+		})
+		It("updates objects' configuration properly", func() {
+			objList, err := DefaultServiceBuilder.BuildObjectList()
+			Expect(err).NotTo(HaveOccurred())
+			for idx, obj := range objList {
+				err = DefaultServiceBuilder.Update(obj)
+				Expect(err).NotTo(HaveOccurred())
+				actual := obj.(*v12.Service)
+				expected := Instance.Spec.ServiceList[idx]
+				Expect(actual.Annotations).To(Equal(expected.Annotations))
+				Expect(actual.Spec).To(Equal(expected.ServiceSpec))
+			}
+		})
+	})
+})
