99116765875087e7ef5fc526d3e89a2053985872
anton.aleksandrov <anton.aleksandrov@jetbrains.com>
2024-04-02 14:49:53 +0200

    TCI-547 reworked deletion of sa if it's removed or renamed

--- internal/resource/serviceaccount.go
+++ internal/resource/serviceaccount.go
@@ -3,6 +3,7 @@ package resource
 import (
 	"context"
 	"fmt"
+	"git.jetbrains.team/tch/teamcity-operator/internal/metadata"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -18,15 +19,18 @@ func (builder *TeamCityResourceBuilder) ServiceAccount() *ServiceAccountBuilder 
 }
 
 func (builder ServiceAccountBuilder) BuildObjectList() ([]client.Object, error) {
-	return []client.Object{
-		builder.getEmptyServiceAccount(),
-	}, nil
+	objectList := []client.Object{}
+	if builder.Instance.ServiceAccountProvided() {
+		objectList = append(objectList, builder.getEmptyServiceAccount())
+	}
+	return objectList, nil
 }
 
 func (builder ServiceAccountBuilder) Update(object client.Object) error {
 	serviceAccount := object.(*v1.ServiceAccount)
 	expected := &builder.Instance.Spec.ServiceAccount
 	serviceAccount.Annotations = expected.Annotations
+	serviceAccount.Labels = metadata.GetLabels(builder.Instance.Name, builder.Instance.Labels)
 	if err := controllerutil.SetControllerReference(builder.Instance, serviceAccount, builder.Scheme); err != nil {
 		return fmt.Errorf("failed setting controller reference: %w", err)
 	}
@@ -34,12 +38,22 @@ func (builder ServiceAccountBuilder) Update(object client.Object) error {
 }
 
 func (builder ServiceAccountBuilder) GetObsoleteObjects(ctx context.Context) ([]client.Object, error) {
-	if !builder.Instance.ServiceAccountProvided() {
-		return []client.Object{
-			builder.getEmptyServiceAccount(),
-		}, nil
+	currentServiceAccountList := &v1.ServiceAccountList{}
+	obsoleteObjects := []client.Object{}
+	listOptions := []client.ListOption{
+		client.InNamespace(builder.Instance.Namespace),
+		client.MatchingLabels(metadata.GetLabels(builder.Instance.Name, builder.Instance.Labels)),
 	}
-	return nil, nil
+	if err := builder.Client.List(ctx, currentServiceAccountList, listOptions...); err != nil {
+		return nil, err
+	}
+	for _, serviceAccount := range currentServiceAccountList.Items {
+		sa := serviceAccount
+		if !builder.Instance.ServiceAccountProvided() || sa.Name != builder.Instance.Spec.ServiceAccount.Name {
+			obsoleteObjects = append(obsoleteObjects, &sa)
+		}
+	}
+	return obsoleteObjects, nil
 }
 
 func (builder ServiceAccountBuilder) UpdateMayRequireStsRecreate() bool {
