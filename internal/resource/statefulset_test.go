7eccee40fdd31f92a4658cb27880925e1a4b519b
anton.aleksandrov <anton.aleksandrov@jetbrains.com>
2024-04-02 15:32:46 +0200

    TCI-547 removed unused function

--- internal/resource/statefulset_test.go
+++ internal/resource/statefulset_test.go
@@ -331,13 +331,3 @@ var _ = Describe("StatefulSet", func() {
 		})
 	})
 })
-
-func RemoveEmptyStrings(s []string) []string {
-	var r []string
-	for _, str := range s {
-		if str != "" {
-			r = append(r, str)
-		}
-	}
-	return r
-}
