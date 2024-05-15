de3ef541c87e825008586cee4339eb99af5bead6
anton.aleksandrov <anton.aleksandrov@jetbrains.com>
2024-05-15 11:23:23 +0200

    TCI-121 added missing permissions for manifest generation

--- internal/controller/teamcity_controller.go
+++ internal/controller/teamcity_controller.go
@@ -53,6 +53,9 @@ type TeamcityReconciler struct {
 //+kubebuilder:rbac:groups=jetbrains.com,resources=teamcities/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=jetbrains.com,resources=teamcities/finalizers,verbs=update
 //+kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
