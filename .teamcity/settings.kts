e45fbc67b32bd253466259652e178d4a0cb87961
anton.aleksandrov <anton.aleksandrov@jetbrains.com>
2026-02-02 11:18:25 +0100

    removed comments

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,11 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.*
 
-/*
- * TeamCity versioned settings entry point.
- * Project hierarchy is defined in _Self/Shared.kt.
- * Credentials are read from DSL context parameters (see .teamcity/README-DSL-CONTEXT.md).
- */
-
 version = "2025.11"
 
 project(_Self.TeamCityOperator)
